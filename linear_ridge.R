f3e26053ebe774e8cc33684befbca405a1e05852
Elaine <flytosky1012@gmail.com>
2023-11-16 00:32:31 +0100

    add code for other questions

---
+++ linear_ridge.R
@@ -0,0 +1,102 @@
+# import data set
+parkinson_data <- read.csv('parkinsons.csv', header = TRUE)
+head(parkinson_data, 5)
+
+#parkinson_data <- scale(parkinson_data)
+
+# Split the data into training and test sets
+n=dim(parkinson_data)[1]
+set.seed(12345)
+id=sample(1:n, floor(n*0.6))
+train_data=parkinson_data[id,]
+test_data=parkinson_data[-id,]
+
+# Scale the data
+scaled_train_data <- as.data.frame(cbind(scale(subset(train_data, select = -c(motor_UPDRS))), motor_UPDRS=train_data$motor_UPDRS))
+scaled_test_data <- as.data.frame(cbind(scale(subset(test_data, select = -c(motor_UPDRS))), motor_UPDRS=test_data$motor_UPDRS))
+
+# linear regression model
+lm_model <- lm(motor_UPDRS ~ ., data = scaled_train_data)
+
+# Training and test MSE
+train_pred <- predict(lm_model, newdata = scaled_train_data)
+train_mse <- mean((scaled_train_data$motor_UPDRS - train_pred)^2)
+cat("The training data's MSE is: ", train_mse)
+
+test_pred <- predict(lm_model, newdata = scaled_test_data)
+test_mse <- mean((scaled_test_data$motor_UPDRS - test_pred)^2)
+cat("The training data's MSE is: ", test_mse)
+
+# Significant variables
+summary(lm_model)
+
+# Log-likelihood function
+log_likelihood_fun <- function(theta, sigma, y, X) {
+  n <- length(y)
+  log_likelihood <- -n/2 * log(2*pi*sigma^2) - 1/(2*sigma^2) * sum((y - X%*%theta)^2)
+  return(log_likelihood)
+}
+
+# Ridge log-likelihood function
+ridge_log_Likelihood_fun <- function(theta, sigma, lambda, y, X) {
+  log_like <- log_likelihood_fun(theta, sigma, y, X)
+  ridge_penalty <- lambda * sum(theta^2)
+  return(-log_like + ridge_penalty)
+}
+
+# Ridge log-likelihood optimization function
+ridge_log_likelihood_opt <- function(lambda, y, X) {
+  start <- c(rep(0, ncol(X)), sd(y)) # combine theta and sigma into one numeric vector
+  theta_length <- ncol(X)
+
+  # Define a function for the optim() call
+  fn_to_optim <- function(params) {
+    theta <- params[1:theta_length]
+    sigma <- params[theta_length + 1]
+    return(-ridge_log_Likelihood_fun(theta, sigma, lambda, y, X))
+  }
+
+  # Call optim()
+  opt_res <- optim(start, fn = fn_to_optim, method = "BFGS")
+
+  # Return theta and sigma separately
+  return(list(theta = opt_res$par[1:theta_length], sigma = opt_res$par[theta_length + 1]))
+}
+
+
+# Degrees of freedom function
+df_fun <- function(lambda, X) {
+  H <- solve(t(X) %*% X + lambda * diag(ncol(X))) %*% t(X) %*% X
+  return(sum(diag(H)))
+}
+
+y <- scaled_train_data$motor_UPDRS
+X <- as.matrix(subset(scaled_train_data, select = -c(motor_UPDRS)))
+# Ridge optimization for different lambdas
+lambdas <- c(1, 100, 1000)
+opt_params <- lapply(lambdas, function(lambda) ridge_log_likelihood_opt(lambda, y, X))
+
+# Predictions and MSE for different lambdas
+ridge_train_preds <- lapply(1:length(opt_params), function(i) {
+  theta <- opt_params[[i]]$theta
+  as.matrix(subset(scaled_train_data, select = -c(motor_UPDRS))) %*% theta
+})
+
+#train_mses <- colMeans((scaled_train_data$motor_UPDRS - ridge_train_preds)^2)
+ridge_train_mse <- sapply(ridge_train_preds, function(pred) mean((pred - scaled_train_data$motor_UPDRS)^2))
+cat("Ridge Training MSE:", ridge_train_mse, "\n")
+
+ridge_test_preds <- lapply(1:length(opt_params), function(i) {
+  theta <- opt_params[[i]]$theta
+  as.matrix(subset(scaled_test_data, select = -c(motor_UPDRS))) %*% theta
+})
+
+ridge_test_mse <- sapply(ridge_test_preds, function(pred) mean((pred - scaled_test_data$motor_UPDRS)^2))
+cat("Ridge Test MSE:", ridge_test_mse, "\n")
+
+
+# Degrees of freedom for different lambdas
+degrees_of_freedom <- sapply(lambdas, function(lambda) df_fun(lambda, X))
+cat("Degrees of Freedom:", degrees_of_freedom, "\n")
+
+
---
+++ logistic.R
@@ -0,0 +1,94 @@
+# Load necessary libraries
+library(ggplot2)
+library(caret)
+
+# import data set
+pima_data <- read.csv('pima-indians-diabetes.csv', header = FALSE)
+colnames(pima_data) <- c("num_of_pregnant", "plasma_glucose_concentration", "blood_pressure",
+                                          "skinfold_thickness", "serum_insulin", "bmi",
+                                          "diabetes_predigree", "age",  "diabetes")
+#head(pima_data, 5)
+
+
+# Scatterplot
+ggplot(pima_data, aes(x=plasma_glucose_concentration, y=age, color=as.factor(diabetes))) +
+  geom_point() +
+  labs(color="diabetes", title='Scatter Plot of Plasma Glucose Concentration on Age')
+
+
+# Logistic Regression
+logsitic_model <- glm(diabetes ~ plasma_glucose_concentration + age, data=pima_data, family=binomial)
+summary(logsitic_model)
+
+# Predict probabilities
+probabilities <- predict(logsitic_model, type="response")
+cat("Probability(Diabetes=1) = 1 / (1 + exp(-(intercept + coef1*x1 + coef2*x2)))")
+
+# Classify observations
+predictions <- ifelse(probabilities >= 0.5, 1, 0)
+
+# Compute misclassification error
+mis_error <- mean(predictions != pima_data$diabetes)
+cat("Misclassification Error:", mis_error, "\n")
+
+# Scatter plot with predicted values
+ggplot(data.frame(pima_data, predictions), aes(x=plasma_glucose_concentration, y=age, color=as.factor(predictions))) +
+  geom_point() +
+  labs(color="predictions",title='Scatter Plot with Predicted Diabetes Values')
+
+
+
+# Decision boundary equation
+# Decision boundary is where the logistic function equals 0.5
+# 0 = intercept + coef1*x1 + coef2*x2
+# x2 = -(intercept + coef1*x1) / coef2
+
+#decision_boundary <- -(coef(logsitic_model)[1] + coef(logsitic_model)[2]*pima_data$age) / coef(logsitic_model)[3]
+decision_boundary_x1 <- pima_data$plasma_glucose_concentration
+decision_boundary_x2 <- -(coef(logsitic_model)[1] + coef(logsitic_model)[2]*decision_boundary_x1) / coef(logsitic_model)[3]
+
+# Add decision boundary to scatter plot
+ggplot(pima_data, aes(x=plasma_glucose_concentration, y=age, color=as.factor(diabetes))) +
+  geom_point() +
+  geom_line(aes(x=decision_boundary_x1, y=decision_boundary_x2), color='red', linetype='dashed', linewidth=1) +
+  labs(color="diabetes", title='Scatter Plot of Plasma Glucose Concentration on Age with Decision Boundary')
+
+# Predictions with different thresholds
+thresholds <- c(0.2, 0.8)
+
+for ( threshold in thresholds) {
+  pred_threshold <- ifelse(predict(logsitic_model, newdata=pima_data, type='response') >= threshold, 1, 0)
+
+  # Scatter plot with predicted values and threshold
+  scatter_plot <- ggplot(data.frame(pima_data, pred_threshold), aes(x=plasma_glucose_concentration, y=age, color=as.factor(pred_threshold))) +
+    geom_point() +
+    labs(color="pred_threshold", title=paste('Scatter Plot with Predicted Diabetes Values (Threshold =', threshold, ')'))
+  print(scatter_plot)
+}
+
+
+# Create new features
+pima_data$z1 <- pima_data$plasma_glucose_concentration^4
+pima_data$z2 <- pima_data$plasma_glucose_concentration^3 * pima_data$age
+pima_data$z3 <- pima_data$plasma_glucose_concentration^2 * pima_data$age^2
+pima_data$z4 <- pima_data$plasma_glucose_concentration * pima_data$age^3
+pima_data$z5 <- pima_data$age^4
+
+# Logistic Regression with new features
+model_expanded <- glm(diabetes ~ plasma_glucose_concentration + age + z1 + z2 + z3 + z4 + z5, data=pima_data, family=binomial)
+
+
+# Predict probabilities and classify observations
+probabilities_expanded <- predict(model_expanded, type="response")
+predictions_expanded <- ifelse(probabilities_expanded >= 0.5, 1, 0)
+
+
+# Scatter plot for the model with expanded features
+ggplot(data.frame(pima_data, predictions_expanded), aes(x=plasma_glucose_concentration, y=age, color=as.factor(predictions_expanded))) +
+  geom_point() +
+  labs(color="predictions_expanded",title='Scatter Plot with Predicted Diabetes Values (Expanded Features)')
+
+
+# Compute misclassification error
+mis_error_expanded <- mean(predictions_expanded != pima_data$diabetes)
+cat("Misclassification Error (Expanded Features):", mis_error_expanded, "\n")
